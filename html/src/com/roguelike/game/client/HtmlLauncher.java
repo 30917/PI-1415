1b7df7d55e409baf5ab04160a8d44f0fd7416474
jm <jm@mj-A>
2014-10-21 18:56:04 +0100

    initial

---
+++ html/src/com/roguelike/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.roguelike.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.roguelike.game.MyGdxGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(800,480);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new MyGdxGame();
+        }
+}
